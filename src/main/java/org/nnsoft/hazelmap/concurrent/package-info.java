8b3f0d1a69a54d398fba2049d072a2cac71f166d
simonetripodi <simone.tripodi@gmail.com>
2012-01-31 16:56:55 +0100

    updated the license headers

--- src/main/java/org/nnsoft/hazelmap/concurrent/package-info.java
+++ src/main/java/org/nnsoft/hazelmap/concurrent/package-info.java
@@ -4,7 +4,7 @@
 package org.nnsoft.hazelmap.concurrent;
 
 /*
- *    Copyright 2011 The 99 Software Foundation
+ *    Copyright 2011 - 2012 The 99 Software Foundation
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
